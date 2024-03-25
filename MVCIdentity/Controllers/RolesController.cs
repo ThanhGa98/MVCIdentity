ce2321db80b4363be78ddb23e0be14f78e25f072
LAPTOP-G87H1VNA\nttha <ntthanh.fit.hcmus@gmail.com>
2024-03-26 01:19:43 +0700

    Add project files.

---
+++ MVCIdentity/Controllers/RolesController.cs
@@ -0,0 +1,64 @@
+﻿using MVCIdentity.App_Start;
+using MVCIdentity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIdentity.Controllers
+{
+    public class RolesController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var rolesList = new List<RoleViewModel>();
+            foreach (var role in _db.Roles)
+            {
+                var roleModel = new RoleViewModel(new ApplicationRole(role.Name,string.Empty));
+                rolesList.Add(roleModel);
+            }
+            return View(rolesList);
+        }
+
+
+        [ClaimsAuthorizeAttribute("A", "ABC", Roles = "Admin")]
+        public ActionResult Create(string message = "")
+        {
+            ViewBag.Message = message;
+            return View();
+        }
+
+
+        [HttpPost]
+        [ClaimsAuthorizeAttribute("A","ABC",Roles = "Admin" )]
+        public ActionResult Create([Bind(Include =
+            "RoleName,Description")]RoleViewModel model)
+        {
+            string message = "That role name has already been used";
+            if (ModelState.IsValid)
+            {
+                var role = new ApplicationRole(model.RoleName, model.Description);
+                var idManager = new IdentityManager();
+
+                if (idManager.RoleExists(model.RoleName))
+                {
+                    return View(message);
+                }
+                else
+                {
+                    idManager.CreateRole(model.RoleName, model.Description);
+                    return RedirectToAction("Index", "Account");
+                }
+            }
+            return View();
+        }
+
+
+        
+        
+    }
+}
---
+++ MVCIdentity/Models/IdentityModels.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.ModelConfiguration;
+using MVCIdentity.App_Start;
+
+namespace MVCIdentity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string Email { get; set; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole,string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
+    {
+        //IdentityConnection defines the LocalDB in the web.config connection string tag
+        public ApplicationDbContext() : base("IdentityConnection")
+        {
+
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public class IdentityManager
+        {
+            // Swap ApplicationRole for IdentityRole:
+            RoleManager<ApplicationRole> _roleManager = new RoleManager<ApplicationRole>(
+                new RoleStore<ApplicationRole>(new ApplicationDbContext()));
+
+            UserManager<ApplicationUser> _userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            ApplicationDbContext _db = new ApplicationDbContext();
+
+
+            public bool RoleExists(string name)
+            {
+                return _roleManager.RoleExists(name);
+            }
+
+
+            public bool CreateRole(string name, string description = "")
+            {
+                // Swap ApplicationRole for IdentityRole:
+                var idResult = _roleManager.Create(new ApplicationRole(name, description));
+                return idResult.Succeeded;
+            }
+
+
+            public bool CreateUser(ApplicationUser user, string password)
+            {
+                var idResult = _userManager.Create(user, password);
+                return idResult.Succeeded;
+            }
+
+
+            public bool AddUserToRole(string userId, string roleName)
+            {
+                var idResult = _userManager.AddToRole(userId, roleName);
+                return idResult.Succeeded;
+            }
+
+
+            public void ClearUserRoles(string userId)
+            {
+                var user = _userManager.FindById(userId);
+                var currentRoles = new List<IdentityUserRole>();
+
+                currentRoles.AddRange(user.Roles);
+                foreach (var role in currentRoles)
+                {
+                    _userManager.RemoveFromRole(userId, _db.Roles.Find(role.RoleId)?.Id);
+                }
+            }
+
+
+            public void RemoveFromRole(string userId, string roleName)
+            {
+                _userManager.RemoveFromRole(userId, roleName);
+            }
+
+
+            public void DeleteRole(string roleId)
+            {
+                var roleUsers = _db.Users.Where(u => u.Roles.Any(r => r.RoleId == roleId));
+                var role = _db.Roles.Find(roleId);
+
+                foreach (var user in roleUsers)
+                {
+                    this.RemoveFromRole(user.Id, role.Name);
+                }
+                _db.Roles.Remove(role);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
+
+
